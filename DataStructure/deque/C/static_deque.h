7a497f063218f3e1c242ad7006f621775620af02
ShinYoung-hwan <syh754200@g.skku.edu>
2023-12-22 16:49:41 +0900

    bug fix about deque

--- DataStructure/deque/C/static_deque.h
+++ DataStructure/deque/C/static_deque.h
@@ -11,38 +11,49 @@ typedef struct{
     int front, rear;
 } Deq;
 
+// 덱 초기화 연산
+// 각 원소를 넣어야할 위치를 지정. 조회 및 삭제 연산시에는 1칸 옆의 값을 조회, 삭제해야함.
 void initDeq(Deq *pdeq){
+    
     pdeq->front = 0;
     pdeq->rear = 1;
 }
+// 덱 가득찼는지 확인하는 연산
 bool isFull(Deq *pdeq){
     return pdeq->front == pdeq->rear;
 }
+// 덱이 비어있는지 확인하는 연산
 bool isEmpty(Deq *pdeq){
     return (pdeq->front + 1) % MAX_DEQ == pdeq->rear;
 }
 
+// 덱의 front에 원소를 삽입하는 연산
 void addFront(Deq *pdeq, Data element){
     if (isFull(pdeq)){
         exit(1);
     }
     pdeq->items[pdeq->front--] = element;
     pdeq->front = pdeq->front >= 0 ? pdeq->front : MAX_DEQ - 1;
-} void addRear(Deq *pdeq, Data element){
+} 
+// 덱의 rear에 원소를 삽입하는 연산
+void addRear(Deq *pdeq, Data element){
     if (isFull(pdeq)){
         exit(1);
     }
     pdeq->items[pdeq->rear] = element;
     pdeq->rear = (pdeq->rear + 1) % MAX_DEQ;
 }
+// 덱의 front에서 원소를 삭제하는 연산
 Data removeFront(Deq *pdeq){
     if (isEmpty(pdeq)){
         exit(1);
     }
-    Data item = pdeq->items[pdeq->front + 1];
     pdeq->front = (pdeq->front + 1) % MAX_DEQ;
+    Data item = pdeq->items[pdeq->front];
     return item;
-} Data removeRear(Deq *pdeq){
+} 
+// 덱의 rear에서 원소를 삭제하는 연산
+Data removeRear(Deq *pdeq){
     if (isEmpty(pdeq)){
         exit(1);
     }
@@ -51,49 +62,17 @@ Data removeFront(Deq *pdeq){
     Data item = pdeq->items[pdeq->rear];
     return item;
 }
+// 덱의 front 원소를 조회하는 연산
 Data peekFront(Deq *pdeq){
     if (isEmpty(pdeq)){
         exit(1);
     }
-    return pdeq->items[pdeq->front + 1];
-} Data peekRear(Deq *pdeq){
+    return pdeq->items[(pdeq->front + 1) % MAX_DEQ];
+}
+// 덱의 rear 원소를 조회하는 연산
+Data peekRear(Deq *pdeq){
     if (isEmpty(pdeq)){
         exit(1);
     }
-    return pdeq->items[pdeq->rear];
-}
-
-void printDeq(Deq *pdeq){
-    // front에서 rear까지 하나하나 출력하기
-    printf("Front(%d) - ", pdeq->front+1);
-
-    int front = (pdeq->front + 1) % MAX_DEQ;
-    while (front != pdeq->rear){
-        printf("%d ", pdeq->items[front]);
-        front = (front + 1) % MAX_DEQ;
-    }
-
-    printf("- Rear(%d)\n", pdeq->rear);
-}
-
-int main(){
-    Deq deq;
-    initDeq(&deq);
-
-    for (int i = 0; i < 9; i++){
-        if (i % 2 == 0){
-            addFront(&deq, i);
-        }
-        else {
-            addRear(&deq, i);
-        }
-    }
-    printDeq(&deq);
-
-    for (int i = 0; i < 5; i++){
-        printf("%d ", removeRear(&deq));
-    }
-    printDeq(&deq);
-
-    return 0;
+    return pdeq->items[(pdeq->rear) > 0 ? pdeq->rear - 1 : MAX_DEQ - 1];
 }
