b1a9fd887b25e2593f57cb07f5447570d51d18c9
ShinYoung-hwan <syh754200@g.skku.edu>
2024-08-05 21:52:45 +0900

    solve at 24.08.05

---
+++ Baekjoon/Problem_2000~2999/Problem_2193/Problem_2193.py
@@ -0,0 +1,14 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    N = int(input())
+    
+    dp = [ 0 ] * (N+1)
+    dp[1] = 1
+    
+    for i in range(2, N+1):
+        dp[i] = dp[i-2] + dp[i-1]
+        
+    print(dp[N])
